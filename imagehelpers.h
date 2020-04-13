3f5a1814d6ce6039f85e378ab508c259a54d119a
jungaha23 <jungah.ahn69@myhunter.cuny.edu>
2020-04-13 06:18:19 -0700

    adding all files to github

---
+++ imagehelpers.h
@@ -0,0 +1,13 @@
+#pragma once
+
+const int MAX_H = 512;
+const int MAX_W = 512;
+
+
+void readImage(std::string filename, int image[MAX_H][MAX_W], int &height, int &width);
+
+void writeImage(std::string filename, int image[MAX_H][MAX_W], int height, int width);
+
+int invert(int original_pixel);
+
+int blur(int a, int b, int c, int d);
