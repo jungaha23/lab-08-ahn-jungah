dc48fb98e149d832d8ce4793758e032353ff0523
Dan <danielelkik@hotmail.com>
2020-04-24 16:39:32 -0400

    added function and original image

--- sample-main.cpp
+++ sample-main.cpp
@@ -16,10 +16,10 @@ int main() {
   // and the 2-dimesional array img contains the image data
 
   // Now we can manipulate the image the way we like
-  
+
   // TASK A
   int out[MAX_H][MAX_W];
-	
+
   for(int row = 0; row < h; row++) {
     for(int col = 0; col < w; col++) {
       out[row][col] = invert(img[row][col]);
@@ -28,10 +28,10 @@ int main() {
 
   // and save this new image to file "taska.pgm"
   writeImage("taska.pgm", out, h, w);
-  
+
   //TASK B
   int invertHalf[MAX_H][MAX_W];
-  
+
   for(int row = 0; row < h; row++) {
     for(int col = 0; col < w/2; col++) { //writes first half of image normally
       invertHalf[row][col] = img[row][col];
@@ -46,9 +46,9 @@ int main() {
 
   //TASK C
   int box[MAX_H][MAX_W];
-  
+
   for(int row = 0; row < h; row++) {
-    for(int col = 0; col < w; col++) { 
+    for(int col = 0; col < w; col++) {
       if ((row > h/4 && row < (h/2 + h/4)) && (col > w/4 && col < (w/2 + w/4))){
       	box[row][col] = 255;
       } else {
@@ -59,12 +59,12 @@ int main() {
 
   // and save this new image to file "taskc.pgm"
   writeImage("taskc.pgm", box, h, w);
-  
+
   //TASK D
   int frame[MAX_H][MAX_W];
-  
+
   for(int row = 0; row < h; row++) {
-    for(int col = 0; col < w; col++) { 
+    for(int col = 0; col < w; col++) {
     	if ((row >= h/4 && row <= (h/2 + h/4)) && (col >= w/4 && col <= (w/2 + w/4))){ //checks if it is the same position as box
 		   if (row == h/4 || row == (h/4 + h/2) || col == w/4 || col == (w/4 + w/2)){ //checks if it is only the frame
 		   	frame[row][col] = 255;
@@ -79,12 +79,12 @@ int main() {
 
   // and save this new image to file "taskd.pgm"
   writeImage("taskd.pgm", frame, h, w);
-  
+
   //TASK E
   int scale[MAX_H][MAX_W];
-  
+
   for(int row = 0; row < h*2; row+=2) {
-    for(int col = 0; col < w*2; col+=2) { 
+    for(int col = 0; col < w*2; col+=2) {
 			scale[row][col] = img[row][col]; //first box
 			scale[row][col+1] = img[row][col]; //box to the right
 			scale[row+1][col] = img[row][col]; //box below
@@ -94,12 +94,12 @@ int main() {
 
   // and save this new image to file "taske.pgm"
   writeImage("taske.pgm", scale, h, w);
-  
+
   //TASK F
   int pixelate[MAX_H][MAX_W];
-  
+
   for(int row = 0; row < h; row+=2) {
-    for(int col = 0; col < w; col+=2) { 
+    for(int col = 0; col < w; col+=2) {
 			pixelate[row][col] = blur(img[row][col], img[row][col+1], img[row+1][col], img[row+1][col+1]);
 			pixelate[row][col+1] = blur(img[row][col], img[row][col+1], img[row+1][col], img[row+1][col+1]);
 			pixelate[row+1][col] = blur(img[row][col], img[row][col+1], img[row+1][col], img[row+1][col+1]);
@@ -109,13 +109,30 @@ int main() {
 
   // and save this new image to file "taskf.pgm"
   writeImage("taskf.pgm", pixelate, h, w);
-<<<<<<< HEAD
-=======
-  
->>>>>>> fa87c3c88b589d89b8873b5a1eb8772717599d18
+
+  //Enhancement by adding a 3 pixel wide "window frame"
+  ////////CANT RUN UNLESS PIXELATE IS COMMENTED OUT//////////////
+  int window[MAX_H][MAX_W];
+  for (int row = 0; row < h; row++){
+    for (int col = 0; col < w; col++){
+      // Check for the vertical middle, the top and bottom of image
+      if (row < 3 || row > h-4 || (row > h/2-2 && row < h/2+2)){
+        //Make these pixels white
+        window[row][col] = 255;
+      }
+      else {
+        // Check for the horizontal middle, the left, and right of the image
+        if ((col > w/2-2 && col < w/2+2) || col < 3 || col > w-4){
+          //Make these pixels white
+          window[row][col] = 255;
+        }
+        else {
+          //Make everything else the regular image
+          window[row][col] = img[row][col];
+        }
+      }
+    }
+  }
+  writeImage("window.pgm",window, h, w);
 
 }
-
-
-
-
